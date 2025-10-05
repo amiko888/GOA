85c005c21ab04fbaea4a72c072e3c21f70c04ab9
amiko888 <davlasheridze2010@gmail.com>
2025-10-05 13:14:50 +0400

    second commit

---
+++ level001/homework.py
@@ -0,0 +1,6 @@
+name = "amiko"
+surname = "davlasheridze"
+age = 15
+height = 182
+
+print("my name is" + " " + name + " " + surname + " " + "i am" + " " + str(age) + " " + "years old" + " " + "and i am" + " " + str(height) + "cm")
