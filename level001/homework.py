7199ad356ce7e5e51ce68e0257cdb64a636a14a1
amiko888 <davlasheridze2010@gmail.com>
2025-10-23 17:47:04 +0400

    fourth commit

--- level001/homework.py
+++ level001/homework.py
@@ -1,6 +1,8 @@
 name = "amiko"
 surname = "davlasheridze"
-age = 15
-height = 182
+age = "15"
+height = "182"
 
-print("my name is" + " " + name + " " + surname + " " + "i am" + " " + str(age) + " " + "years old" + " " + "and i am" + " " + str(height) + "cm")
+print("my name is" + " " + name + " " + surname + " " + "i am" + " " + age + " " + "years old" + " " + "and i am" + " " + height + "cm")
+
+
